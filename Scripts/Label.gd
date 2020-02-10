912a0b2520ebe8f94db8de230760f13228693f67
haoyu lin <59937983+haoylin@users.noreply.github.com>
2020-02-10 18:16:49 -0500

    finish assignment

--- Scripts/Label.gd
+++ Scripts/Label.gd
@@ -5,4 +5,8 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	pass
+	if Input.is_action_pressed("left"):
+		pass
+		var mouse_pos = get_viewport().get_mouse_position()
+		print(mouse_pos.x)
+		text = str(mouse_pos.x) + ", " + str(mouse_pos.y)
